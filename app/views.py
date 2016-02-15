7a635b01cbaeef6770b7bc5b373e128648f81db7
J.B. White <jbwhite85@gmail.com>
2016-02-14 23:56:52 -0500

    updated views.py

--- app/views.py
+++ app/views.py
@@ -4,16 +4,20 @@ import io
 from django.conf import settings
 import os
 
-
 # Create your views here.
 
-def input(user):
-	fname = raw_input "First name: "
-	lname = raw_input "Last name: "
-	street = raw_input "Street Address: (ie: 200 7th Ave.)"
-	cityst = raw_input "City, ST: (ie: New York, NY)"
-	zipcode = raw_input "Zipcode: "
-	email = raw_input "Email address: "
-	phone = raw_input "Area Code + Phone Number: (ie: 555-555-5555)"
+fname = input("First name: ")
+lname = input("Last name: ")
+street = input("Street Address: (ie: 200 7th Ave.)")
+cityst = input("City, ST: (ie: New York, NY)")
+zipcode = input("Zipcode: ")
+email = input("Email address: ")
+phone = input("Area Code + Phone Number: (ie: 555-555-5555)")
 
-	
+print (fname, lname)
+print (street)
+print (cityst, zipcode)
+print (email)
+print (phone)
+
+print ("Please ensure all of your information correct?  ")
