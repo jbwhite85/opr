2972337ef5e63a72b0288cba34eab17d6d293e62
Andy McCurdy <andy@andymccurdy.com>
2016-02-21 14:37:56 -0800

    django 1.6+ cleanup

--- api/views.py
+++ api/views.py
@@ -1,15 +1,13 @@
-from django.shortcuts import redirect, render, get_object_or_404, render_to_response
+from django.shortcuts import redirect, render, get_object_or_404
 from .forms import UserForm
 from .models import User
-from django.http import HttpResponseRedirect
 from django.contrib import auth
-from django.core.context_processors import csrf
+
 
 # Create your views here.
 def login(request):
-    c = {}
-    c.update(csrf(request))
-    return render_to_response('login.html', c)
+    return render(request, 'login.html', {})
+
 
 def auth_view(request):
     username = request.POST.get('username', '')
@@ -18,22 +16,23 @@ def auth_view(request):
 
     if user is not None:
         auth.login(request, user)
-        return HttpResponseRedirect('/accounts/loggedin')
+        return redirect('loggedin')
     else:
-        return HttpResponseRedirect('/accounts/invalid')
+        return redirect('invalid_login')
+
 
 def loggedin(request):
-    return render_to_response('loggedin.html',
-                                {'full_name': request.user.username})
+    return render(request, 'loggedin.html',
+                  {'full_name': request.user.username})
+
 
 def invalid_login(request):
-    return render_to_response('invalid_login.html')
+    return render(request, 'invalid_login.html', {})
+
 
 def logout(request):
     auth.logout(request)
-    return render_to_response('logout.html')
-
-
+    return render(request, 'logout.html', {})
 
 
 def create_user(request):
